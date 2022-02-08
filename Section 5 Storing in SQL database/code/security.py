1275a522e7a4a35c4dd951537c6131a80950e770
ArneDeCnodder <r0657784@student.thomasmore.be>
2022-02-08 15:54:59 +0100

    section 5 done

---
+++ Section 5 Storing in SQL database/code/security.py
@@ -0,0 +1,35 @@
+from user import User
+
+#users = [
+    #User(1,'bob','password')
+    #{
+    #    'id':1,
+    #    'username':'bob',
+    #    'password': 'password'
+    #}
+#]
+
+#username_mapping = {u.username: u for u in users} #{ 'bob':
+    #{
+    #    'id':1,
+    #    'username':'bob',
+    #    'password': 'password'
+    #}
+#}
+
+#userid_mapping = {u.id: u for u in users}
+#userid_mapping = { 1: {
+#    'id':1,
+#    'username':'bob',
+#    'password': 'password'
+#}}
+
+def authenticate(username, password):
+    user = User.find_by_username(username)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    #return userid_mapping.get(user_id,None)
+    return User.find_by_id(user_id)
